db14a95274710abf3a3c96b8b95e14905f6a6be8
Roy Lee <keymasroy@gmail.com>
2024-05-10 16:00:57 +0900

    typecheck 스크립트 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,26 @@
 module.exports = {
   root: true,
-  extends: [
-    '@nuxt/eslint-config'
-  ],
+  extends: ['@nuxt/eslint-config'],
   rules: {
     // Global
     semi: ['error', 'never'],
-    quotes: ['error', 'single'],
+    quotes: ['warn', 'single'],
     'quote-props': ['error', 'as-needed'],
+    'no-empty': 'warn',
+    'no-constant-condition': 'warn',
+
+    // typescript
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     // Vue
-    'vue/multi-word-component-names': 0,
+    'vue/attribute-hyphenation': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/multi-word-component-names': 'off',
     'vue/max-attributes-per-line': 'off',
-    'vue/no-v-html': 0
-  }
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/v-on-event-hyphenation': 'off',
+    'vue/no-unused-vars': 'warn',
+  },
 }
