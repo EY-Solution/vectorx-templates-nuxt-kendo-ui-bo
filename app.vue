029d4feced0f003f235ed67fb3ef99684d5add3a
Roy Lee <keymasroy@gmail.com>
2024-05-20 16:42:52 +0900

    프레임워크 v24.1.5-alpha.9 버전 적용

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
   <VxManagementApp :language="language" :locale="locale" />
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
+
 const language = ref($vx.env.appProps.nuxt.kendoUi.i18n.message.default)
 const locale = ref($vx.env.appProps.nuxt.kendoUi.i18n.locales[0])
 </script>
